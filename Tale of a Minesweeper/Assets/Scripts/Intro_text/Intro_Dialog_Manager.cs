cf29522ae06d3396654bd5a34ee63c231e994ddc
fikson <filip.matic@mail.com>
2019-10-10 21:20:58 +0200

    animacije i generisanje levela

---
+++ Tale of a Minesweeper/Assets/Scripts/Intro_text/Intro_Dialog_Manager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Intro_Dialog_Manager : MonoBehaviour
+{
+
+    public Text Dialog_Text;
+
+    private Queue<string> Manager_Sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Manager_Sentences = new Queue<string>();
+
+    }
+
+    public void Start_Dialouge(Intro_Dialog dialog)
+    {
+
+        Manager_Sentences.Clear();
+
+        foreach (string sentence in dialog.Dialog_Sentences)
+        {
+
+            Manager_Sentences.Enqueue(sentence);
+
+        }
+
+        Display_Next_Sentence();
+
+    }
+
+    public void Display_Next_Sentence()
+    {
+
+        if (Manager_Sentences.Count == 0)
+        {
+
+            End_Dialog();
+            return;
+
+        }
+
+        string sentence = Manager_Sentences.Dequeue();
+
+        StopAllCoroutines();
+        StartCoroutine(Type_Sentence(sentence));
+
+    }
+
+    IEnumerator Type_Sentence (string sentence)
+    {
+        Dialog_Text.text = "";
+
+        foreach(char letter in sentence.ToCharArray())
+        {
+            Dialog_Text.text += letter;
+            yield return new WaitForSecondsRealtime(0.055f);
+        }
+
+    }
+    
+    void End_Dialog()
+    {
+
+        Debug.Log("End");
+
+    }
+
+    
+    
+}
---
+++ Tale of a Minesweeper/Assets/Scripts/Intro_text/Intro_Dialog_Triger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_Dialog_Triger : MonoBehaviour
+{
+    public Intro_Dialog dialog;
+
+    public void Start()
+    {
+
+        Invoke("Trigger_Dialogue", 2f);
+
+    }
+
+
+    public void Trigger_Dialogue()
+    {
+
+        FindObjectOfType<Intro_Dialog_Manager>().Start_Dialouge(dialog);
+
+        Invoke("Invoce_Next", 15f);
+
+
+    }
+
+    private void Invoce_Next()
+    {
+
+        FindObjectOfType<Intro_Dialog_Manager>().Display_Next_Sentence();
+
+        Invoke("Invoce_Next", 15f);
+
+    }
+}
